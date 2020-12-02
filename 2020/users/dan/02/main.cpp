c885c462aff014af127ed884bf0ca9b2203b83fd
Daniel Papke <papked@gmail.com>
2020-12-02 06:24:43 +0000

    Updated to use gcc 10.1. Switched to ranges for algorithm usage.

--- 2020/users/dan/02/main.cpp
+++ 2020/users/dan/02/main.cpp
@@ -28,8 +28,8 @@ int main()
       char tossMe;
       Pass p;
       istringstream ss(line);
+      // Format: %d-%d %c: %s
       ss >> p.min >> tossMe >> p.max >> p.letter >> tossMe >> p.password;
-      //sscanf(line.data(), "%d-%d %c: %s", &p.min, &p.max, &p.letter, &begin(p.password));
       entries.push_back(p);
    }
 
@@ -40,18 +40,18 @@ int main()
    //}
 
    // Part 1
-   const auto validPassCount = count_if(begin(entries), end(entries),
+   const auto validPassCount = ranges::count_if(entries,
       [](Pass p){
-         const auto lCount = count(begin(p.password), end(p.password), p.letter);
+         const auto lCount = ranges::count(p.password, p.letter);
          return p.min <= lCount
             && p.max >= lCount;
       });
 
-   std::cout << "Valid passwords: " << validPassCount << std::endl;
+   std::cout << "Valid passwords (part 1): " << validPassCount << std::endl;
 
    // Part 2
    // Validate whether the password contains a letter in either (but not both) of the positions (1-based)
-   const auto validPass2 = count_if(begin(entries), end(entries),
+   const auto validPass2 = ranges::count_if(entries,
       [](Pass p){
          const bool firstPos = p.password[p.min-1] == p.letter;
          const bool secPos = p.password[p.max-1] == p.letter;
--- 2020/users/dan/build-pi.sh
+++ 2020/users/dan/build-pi.sh
@@ -3,4 +3,4 @@
 curr=$( pwd )
 int_code=""
 if [ ! -z "$1" ]; then int_code="../intcode/*.cpp"; fi
-g++-9.1 $curr/*.cpp $int_code -O2 -std=c++2a -Wall -Wno-psabi -o $curr/test.exe
+g++-10.1 $curr/*.cpp $int_code -O2 -std=c++20 -Wall -Wno-psabi -o $curr/test.exe
