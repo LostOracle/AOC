7b8cd9359bf15201c5bd349e1d66d4b21dcc0806
Caleb Jamison <cbjamo@gmail.com>
2020-12-03 01:04:16 -0500

    More rusty day 2

--- 2020/users/cbjamo/day2/src/main.rs
+++ 2020/users/cbjamo/day2/src/main.rs
@@ -1,10 +1,8 @@
-use std::fs::File;
-use std::io::BufReader;
-use std::io::prelude::*;
-use std::path::Path;
+use std::fs;
+use std::str::FromStr;
 #[macro_use] extern crate scan_fmt;
 
-fn day1( values: &Vec<Password> ) -> i32 {
+fn part1( values: &Vec<Password> ) -> i32 {
     let mut ret = 0;
     for val in values {
         let count = val.pass.matches(val.letter).count();
@@ -19,7 +17,7 @@ fn day1( values: &Vec<Password> ) -> i32 {
     ret
 }
 
-fn day2( values: &Vec<Password> ) -> i32 {
+fn part2( values: &Vec<Password> ) -> i32 {
     let mut ret = 0;
     for val in values {
         if (val.pass.chars().nth(val.min-1).unwrap() == val.letter) ^ (val.pass.chars().nth(val.max-1).unwrap() == val.letter) {
@@ -38,27 +36,22 @@ struct Password {
     pass: String
 }
 
-fn main() {
-    let path = Path::new("input");
-    //let display = path.
-    
-    let file = match File::open(&path) {
-        Err(why) => panic!("Couldn't open {}: {}", path.display(), why),
-        Ok(file) => file,
-    };
+impl FromStr for Password {
+    type Err = ();
 
-    let mut values = Vec::new();
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let (min, max, letter, pass) = scan_fmt!(s, "{d}-{d} {}: {}", usize, usize, char, String).unwrap();
 
-    for line in BufReader::new(file).lines() {
-        let line = line.unwrap();
-
-        let (min, max, letter, pass) = scan_fmt!(&line, "{d}-{d} {}: {}", usize, usize, char, String).unwrap();
-        let password = Password{ min, max, letter, pass };
-
-        values.push(password);
+        Ok(Self{ min, max, letter, pass } )
     }
+}
 
-    println!( "{}", day1(&values) );
-    println!( "{}", day2(&values) );
+fn main() {
+    let file = fs::read_to_string("input").expect("File not found");
+
+    let values: Vec<Password> = file.trim().lines().map(|s| s.parse().unwrap()).collect();
+
+    println!( "Part 1: {}", part1(&values) );
+    println!( "Part 2: {}", part2(&values) );
 }
 
